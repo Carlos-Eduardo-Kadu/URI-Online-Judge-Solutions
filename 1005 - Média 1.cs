53153c7905172692f5b4bf181e53ef2e28033e9c
Kadu <kadumeloduarte@gmail.com>
2025-05-07 11:28:07 -0300

    1005 - Média 1.cs

---
+++ 1005 - Média 1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+
+namespace ExeUri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            double a, b, media;
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            media = (a * 3.5 + b * 7.5) / 11.0;
+            
+            Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
