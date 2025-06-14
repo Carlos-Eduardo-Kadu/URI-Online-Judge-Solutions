58adf1f05733cf30218d2269dc3d0d2ea3492e73
Kadu <kadumeloduarte@gmail.com>
2025-06-14 13:43:25 -0300

    1153 - Fatorial Simples.cs

--- 1153 - Fatorial Simples.cs
+++ 1153 - Fatorial Simples.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Globalization;
 
+namespace ExeUri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, y = 1;
+
+            n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++) 
+            {
+                y = y * i;
+            }
+            Console.WriteLine(y);
+
+        }
+    }
+}
