2ce37e8789e1598d1597101e4fac2f6ff5f6805d
Kadu <kadumeloduarte@gmail.com>
2025-07-09 17:02:36 -0300

    2747 - Saída 1.cs

---
+++ 2747 - Saída 1.cs
@@ -0,0 +1,19 @@
+using System; 
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new string('-', 39));
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("|" + new string(' ', 37) + "|");
+        }
+
+        Console.WriteLine(new string('-', 39));
+
+    }
+
+}
