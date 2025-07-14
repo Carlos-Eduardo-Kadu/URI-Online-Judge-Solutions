6cd1fc2fc22d8553b0ca28b429b83a410443ea93
Kadu <kadumeloduarte@gmail.com>
2025-07-14 14:29:22 -0300

    2862 - Inseto!.cs

---
+++ 2862 - Inseto!.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int C = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < C; i++)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            if (N > 8000)
+            {
+                Console.WriteLine("Mais de 8000!");
+            }
+            else 
+            {
+                Console.WriteLine("Inseto!");
+            }
+        }
+    }
+}
